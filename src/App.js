1d3e4fa1c43397902420e0c55b03fa63968e6584
lenyyx13 <emilia.czopik@student.put.poznan.pl>
2025-06-22 21:20:30 +0200

    haha

--- src/App.js
+++ src/App.js
@@ -12,18 +12,23 @@ function App() {
     <div className="App">
       <header  className="App-header">
         <div id="test" className="header">
-          <a href="#">Projekty</a>
-          <a href="#">o mnie</a>
-          <a href="#">CV</a>
-          <a href="#" className="text-header">Kontakt</a>
+          <a href="/">Projekty</a>
+          <a href="/test">o mnie</a>
+          <a href="/test">CV</a>
+          <a href="/test" className="text-header">Kontakt</a>
         </div>
       </header>
       <section className="hejkanaklejka">
-        <h1 className="duzynapis">PORTFOLIO</h1>
-        <p className="imie">Emilia Czopik</p>
-        <p className="textpod">Projektuję przestrzenie, które zostają na dłużej.</p>
-        <p className="textpod">Zarówno wewnątrz, jak i na zewnątrz.</p>
-        <p className="textpod margin">Architektura • Wnętrza • Przestrzeń publiczna</p>
+        <div className='col-12 col-lg-6 '>
+          <div className='testest'>
+             <h1 className="duzynapis">PORTFOLIO</h1>
+            <p className="imie">Emilia Czopik</p>
+            <p className="textpod">Projektuję przestrzenie, które zostają na dłużej.</p>
+            <p className="textpod">Zarówno wewnątrz, jak i na zewnątrz.</p>
+            <p className="textpod margin">Architektura • Wnętrza • Przestrzeń publiczna</p>
+          </div>
+        </div>
+        
       </section>
       <section className="projekty">
        <div className="didplayflex">
--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,12 +1,21 @@
+
+
 function header() {
     const header = document.getElementById('test');
     const projekt = document.getElementsByClassName('projekty')[0];
     let lastScrollTop = 0;
-
+    let hoverMyszka = true;
     const pełnaWysokość = header.scrollHeight;
+    const arrow = document.createElement('img');
+    arrow.src = '/arrow.png';
+    arrow.alt = 'Scrolluj w dół';
+    arrow.classList.add('scroll-arrow');
+    projekt.appendChild(arrow);
+
     header.style.maxHeight = `${pełnaWysokość}px`;
 
     projekt.addEventListener('scroll', () => {
+        hoverMyszka = false;
         const currentScrollTop = projekt.scrollTop;
 
         if (currentScrollTop > lastScrollTop) {
@@ -14,10 +23,32 @@ function header() {
             header.style.opacity = '0';
         } else if (currentScrollTop < lastScrollTop) {
             header.style.maxHeight = `${pełnaWysokość}px`;
-            header.style.opacity = '1';
+            header.style.opacity = '0.8';
         }
 
         lastScrollTop = currentScrollTop;
+
+        if (!hoverMyszka && arrow.parentElement) {
+            arrow.style.opacity = '0';
+            setTimeout(() => {
+                if (arrow.parentElement) {
+                    arrow.remove();
+                }
+            }, 500);
+        }
+    });
+
+
+    const links = header.querySelectorAll('a');
+    const currentPath = window.location.pathname;
+
+    links.forEach(link => {
+        const linkPath = new URL(link.href).pathname;
+        if (linkPath === currentPath) {
+        link.classList.add('active');
+        } else {
+        link.classList.remove('active');
+        }
     });
 }
 
